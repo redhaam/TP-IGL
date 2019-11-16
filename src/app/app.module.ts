603c34996e582da7f00f9c9b27b7fc034252686a
kadem khadidja <hk_kadem@esi.dz>
2019-11-16 12:05:19 +0100

    creating a basic authentification interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthentificationComponent
   ],
   imports: [
     BrowserModule,
