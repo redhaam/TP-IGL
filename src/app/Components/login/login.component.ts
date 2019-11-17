109ed8a7b70ca705bae469a0e24fae87947534e4
iDOVICSA <hm_kadri@esi.dz>
2019-11-18 00:45:06 +0100

    service file / appjs/serverjs  added

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,18 +1,24 @@
-import { Component, ViewChild, OnInit, Input } from "@angular/core";
+import { Component, ViewChild, OnInit, Input } from '@angular/core';
 import { User } from '../../models/user';
+import { NgForm } from '@angular/forms';
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
   @Input() user: User = {
-    email: "",
-    password: ""
+    email: '',
+    password: ''
   };
-  @ViewChild("userForm", null) form: any;
-  constructor() {}
+  @ViewChild('userForm', null) form: any;
+  constructor() { }
 
-  ngOnInit() {}
-  authentification({ value, valid }: { value: any; valid: boolean }) {}
+
+  ngOnInit() { }
+  authentification(form: NgForm) {
+    if ((form.value.email === 'hm_kadri@esi.dz') && (form.value.password === '1234')) {
+      alert('Login success');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './Components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
