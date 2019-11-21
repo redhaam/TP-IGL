43c6a0cf96a7f6c46b4dbafbd1c8978487d0a946
kadem khadidja <hk_kadem@esi.dz>
2019-11-21 13:14:12 +0100

    ui feedback

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./Components/login/login.component";
+import { UiFeedbackComponent } from "./Components/ui-feedback/ui-feedback.component";
 
 const routes: Routes = [
   {
     path: "",
-    component: LoginComponent
+    component: /* LoginComponent */ UiFeedbackComponent
   }
 ];
 
