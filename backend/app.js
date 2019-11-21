cdccdc1daadcd0c0135a68fae7ef97ba65b5bfda
iDOVICSA <hm_kadri@esi.dz>
2019-11-21 12:21:30 +0100

    allowing access control

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    "Access-Control-Allow-Header",
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.setHeader(
