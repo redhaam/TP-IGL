63ef6a62e9ca6c73e41f3d8c49140ebc4c76e97e
iDOVICSA <hm_kadri@esi.dz>
2020-01-03 23:54:49 +0100

    Updating Selenium Script

--- seleniumScript.js
+++ seleniumScript.js
@@ -3,12 +3,37 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
 (async function example() {
   let driver = await new Builder().forBrowser("chrome").build();
   try {
-    await driver.get("http://localhost:4200/ajout-etudiant");
+    await driver.get("http://localhost:4200");
+
+    await driver
+      .findElement(
+        By.xpath(
+          "/html/body/app-root/app-login/div/div/form/div[1]/div[3]/label"
+        )
+      )
+      .click();
 
     await driver
       .findElement(By.xpath('//*[@id="email"]'))
-      .sendKeys("reda@gmail.com");
-    await driver.findElement(By.xpath('//*[@id="nom"]')).sendKeys("reda");
+      .sendKeys("hm_kadri@esi.dz");
+
+    await driver.findElement(By.xpath('//*[@id="password"]')).sendKeys("1234");
+    await driver
+      .findElement(By.xpath("/html/body/app-root/app-login/div/div/form/input"))
+      .click();
+
+    await driver
+      .findElement(
+        By.xpath(
+          "/html/body/app-root/app-ui-admin/div/div/div[2]/div/div[2]/div/a"
+        )
+      )
+      .click();
+    await driver
+      .findElement(By.xpath('//*[@id="email"]'))
+      .sendKeys("amirouche@esi.dz");
+
+    await driver.findElement(By.xpath('//*[@id="nom"]')).sendKeys("Redha");
     await driver
       .findElement(By.xpath('//*[@id="prenom"]'))
       .sendKeys("amirouche");
@@ -18,7 +43,31 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
         By.xpath("/html/body/app-root/app-student-form/div/form/input")
       )
       .click();
-    await driver.get("http://localhost:4200/feedback");
+    await driver.findElement(By.xpath('//*[@id="""navbarDropdown"]')).click(); // clique sue déconnexion
+
+    // le login a nouveau par un compte etudiant qu'on vient de créer
+    await driver
+      .findElement(
+        By.xpath(
+          "/html/body/app-root/app-login/div/div/form/div[1]/div[3]/label"
+        )
+      )
+      .click();
+
+    await driver
+      .findElement(By.xpath('//*[@id="email"]'))
+      .sendKeys("amirouche@esi.dz");
+
+    await driver.findElement(By.xpath('//*[@id="password"]')).sendKeys("12546"); // le password est par défault 12546 lors de l'ajout d'etudiant
+    await driver
+      .findElement(By.xpath("/html/body/app-root/app-login/div/div/form/input"))
+      .click();
+    await driver
+      .findElement(
+        By.xpath("/html/body/app-root/app-ui-etudiant/div/div/div/div[3]/div/a")
+      ) // clique sur la button ajouter feedback
+      .click();
+
     await driver.findElement(By.xpath('//*[@id="module"]')).sendKeys("THP");
     await driver.findElement(By.xpath('//*[@id="niveau"]')).sendKeys("1CS");
     await driver
@@ -33,10 +82,10 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
     console.log(err);
   } finally {
     console.log("Success");
-    try {
+    /* try {
       await driver.quit();
     } catch (error) {
       console.log(`error quitting selenium`);
-    }
+    }*/
   }
 })();
